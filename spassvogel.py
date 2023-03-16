8dafafdaa338bf0ad4e31f8467c721616df78d4a
Eichenmann <wollmannmarcel1107@gmail.com>
2023-03-16 14:51:04 +0100

    Weltbeste Witze

---
+++ spassvogel.py
@@ -0,0 +1,11 @@
+''' weltbeste Witze'''
+import random
+Witze = ["Hast du ein Bad genommen? – Warum, fehlt eins?","Egal, wie gut du schläfst: Albert schläft wie Einstein", "Wissenschaftler haben herausgefunden – und sind wieder reingegangen.",
+          "Sitzt einer im Stehcafé.", "Wie heißt der Schutzpatron der Vergesslichen? – Dings", "Hast du was zu trinken? – Wasser. – Nee, was Härteres! – Ok, Eis.",
+            "Was ist rot und steht im Wald? – Ein Kirsch", "Was ist das Gegenteil von Reformhaus? – Reh hinterm Haus", 
+            "Was ist weiß und rollt den Berg rauf? – Eine Lawine mit Heimweh", "Wie nennt man einen Bumerang, der nicht zurückkommt? – Stock",
+              "Wie nennt man ein Delfin in Unterhose? – Slipper", "Egal wie voll du bist, Rudi war Völler."]
+       
+
+Zufallswitz = random.choice(Witze)
+print ("weltbester Witz:", (Zufallswitz))
